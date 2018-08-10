4ecb9b49a4e1f08b3fcdee3959ea1a1995226960
Courtney Haltiwanger <chalt0319@gmail.com>
2018-08-10 15:44:47 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -4,16 +4,16 @@ class FiguresController < ApplicationController
     erb :'/figures/index'
   end
 
-  get '/figure/:id' do
-    @fig = Figure.find(params[:id])
-    erb :'/figures/show'
-  end
-
   get '/figure/new' do
     erb :'/figures/new'
   end
 
   post '/figres/new' do
 
+  end
+
+  get '/figure/:id' do
+    @fig = Figure.find(params[:id])
+    erb :'/figures/show'
   end
 end
